f06fc9f61b63c5110d6fc00acf078d8fb9c7593d
varjangn <varjangnandaniya8@gmail.com>
2026-06-25 15:40:40 +0530

    initial commit with django apps

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from djroute!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ maproute/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MaprouteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'maproute'
